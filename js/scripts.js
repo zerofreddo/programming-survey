548041bcb8ad7df0a9adf5386433982e03629cf5
Ross Beckman <aguywithgames1212@gmail.com>
2020-02-23 17:12:15 -0800

    set if else statements and ather input values from form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-function onSubmit(){
-  if
-}
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    var q1 = parseInt($("input:radio[name=q1]:checked").val());
+    var q2 = parseInt($("input:radio[name=q2]:checked").val());
+    var q3 = parseInt($("input:radio[name=q3]:checked").val());
+    var q4 = parseInt($("input:radio[name=q4]:checked").val());
+    var q5 = parseInt($("input:radio[name=q5]:checked").val());
+    var score = q1 + q2 + q3 + q4 + q5;
+    if (score <=5) {
+      $(".javascript").show();
+    }
+
+  });
+});
